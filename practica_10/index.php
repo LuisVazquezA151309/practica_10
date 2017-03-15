7886cc405f7ad2cb75a4a2776f799218ba1412b2
LuisVazquezA151309 <a151309@outlook.com>
2017-03-14 20:26:39 -0600

    Add files via upload

---
+++ practica_10/index.php
@@ -0,0 +1,41 @@
+<?Php
+// comentarios
+#comentarios
+/* comentarios multilineal*/
+
+echo "Hola mundo php";
+?>
+<html>
+<head>
+<title>
+Practica 10 php
+</title>
+</head>
+<body>
+<h1>Hola Mundo html </h1>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practica_10/index_2.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>
+Practica 10 php
+</title>
+</head>
+<body>
+<h1> Usuario php </h1>
+<p><?php
+echo "parrafo php";
+?></p>
+<p>Parrafo html</p>
+<h3>
+<?php
+echo "Parrafo php"</p>
+?>
+</h3>
+</body>
+</html>
+
+
